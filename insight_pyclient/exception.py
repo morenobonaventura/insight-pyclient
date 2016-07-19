628efe1ac69564b5604079c9ac2b9502004aef68
Thibault de Balthasar <contact@thibaultdebalt.fr>
2016-07-19 14:45:53 +0100

    Add new type of exception

--- insight_pyclient/exception.py
+++ insight_pyclient/exception.py
@@ -6,7 +6,20 @@
 """
 
 
-class APIException(Exception):
+class InsightPyClientException(Exception):
+    """
+    This exception will never be thrown in the first place. It is done in purpose that the other custom exceptions \
+    inherit from it so, it will be easier later to deal with the module exceptions.
+
+    @ivar message: The message of the exception
+    @type message: String
+    """
+
+    def __init__(self, message):
+        super(Exception, self).__init__(message)
+
+
+class APIException(InsightPyClientException):
     """
     This Exception will be thrown in case of error within the API calls.
 
@@ -19,7 +32,21 @@ class APIException(Exception):
     """
 
     def __init__(self, message, code, ret):
-        super(Exception, self).__init__(message)
+        super(InsightPyClientException, self).__init__(message)
         self.code = code
         self.ret = ret
         self.message = message
+
+
+class ParamException(InsightPyClientException):
+    """
+    This exception will be raised if the arguments given to a function or method are wrong.
+
+    @ivar message: The message of the exception
+    @type message: String
+    """
+
+    def __init__(self, message):
+        super(InsightPyClientException, self).__init__(message)
+        self.message = message
+
