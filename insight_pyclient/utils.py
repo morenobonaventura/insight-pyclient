6a8002515db22d1613a95a15bd9087b22f064cc3
Thibault de Balthasar <contact@thibaultdebalt.fr>
2016-07-19 15:33:03 +0100

    Adding conversion functions

---
+++ insight_pyclient/utils.py
@@ -0,0 +1,30 @@
+# -*- coding:Utf-8 -*
+"""
+Will contain useful functions for the rest of the module
+
+@author: Thibault de Balthasar
+@contact: contact (at) thibaultdebalt [.] fr
+@license: GNU GENERAL PUBLIC LICENSE Version 3
+"""
+
+
+def satoshi_to_bitcoin(satoshis):
+    """
+    Converts the given value from Satoshi to Bitcoin
+    @param satoshis: The amount to convert
+    @param satoshis: int
+    @return: The converted value
+    @rtype: Float
+    """
+    return float(satoshis) / 100000000.0
+
+
+def bitcoin_to_satoshi(bitcoins):
+    """
+        Converts the given value from Bitcoin to Satoshi
+        @param bitcoins: The amount to convert
+        @param bitcoins: int
+        @return: The converted value
+        @rtype: Intr
+        """
+    return int(bitcoins * 100000000)
